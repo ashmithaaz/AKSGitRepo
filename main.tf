c91ed3a4e13d381f7f53c72dc74f52cbb12ab29c
ashmithaaz <117173742+ashmithaaz@users.noreply.github.com>
2022-11-01 18:34:36 +0000

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,56 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "default" {
+  name     = "tamops"
+  location = "East US 2"
+
+  tags = {
+    environment = "Production"
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "akscontainerRegistry1233"
+  resource_group_name = azurerm_resource_group.default.name
+  location            = "East US 2"
+  sku                 = "Standard"
+  admin_enabled       = true
+}
+
+resource "azurerm_kubernetes_cluster" "akscluster1" {
+  name                = "cluster-aks1233"
+  location            = "East US 2"
+  resource_group_name = azurerm_resource_group.default.name
+  dns_prefix          = "exampleaks12"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "standard_d2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Production"
+  }
+}
+
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "acr_admin_username" {
+  value     = azurerm_container_registry.acr.admin_username
+  sensitive = true
+}
+
+output "acr_admin_password" {
+  value     = azurerm_container_registry.acr.admin_password
+  sensitive = true
+}
